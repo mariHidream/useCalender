613dd0a1b6c5ad63a19b648197f6ce2a834951a1
mari <uiop6452@gmail.com>
2024-01-04 05:12:27 -0800

    다이어리 리스트업

--- src/App.js
+++ src/App.js
@@ -1,42 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
 import './App.css';
 
-const dummyList = [
-  {
-    id : 1,
-    author : "마리",
-    content : "hi",
-    emotion : 3,
-    created_date : new Date().getTime()
-  },{
-    id : 2,
-    author : "마리2",
-    content : "hi2",
-    emotion : 2,
-    created_date : new Date().getTime()
-  },{
-    id : 3,
-    author : "마리3",
-    content : "hi3",
-    emotion : 4,
-    created_date : new Date().getTime()
-  },{
-    id : 4,
-    author : "마리4",
-    content : "hi4",
-    emotion : 1,
-    created_date : new Date().getTime()
-  }
-]
-
 function App() {
+
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0)
+
+  const onCreate = (author,content,emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id : dataId.current,
+    };
+    dataId.current += 1;
+
+    setData([newItem, ...data])
+  }
+
+
   return (
     <div className='App'>
-      <DiaryEditor />
-      <DiaryList diary={dummyList}/>
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diary={data}/>
     </div>
   );
 }
--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,7 +1,7 @@
 
 import { useRef, useState } from "react";
 
-const DiaryEditor = () => {
+const DiaryEditor = ({onCreate}) => {
     const authorInput = useRef();
     const contentInput = useRef();
     const [state, setState]  = useState({
@@ -26,7 +26,13 @@ const DiaryEditor = () => {
             contentInput.current.focus()
             return
         }
+        onCreate(state.author, state.content, state.emotion);
         alert("저장성공")
+        setState({
+            author : "",
+            content : "",
+            emotion : 1
+        })
     }
     
     return (
