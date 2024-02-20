430dd90e5d46f83eb5b5dac13f0d0fe32927f46b
mari <uiop6452@gmail.com>
2024-02-20 22:41:41 +0900

    2/19일자공부

--- src/App.js
+++ src/App.js
@@ -35,10 +35,33 @@ export const DiaryStateContext = React.createContext();
 
 export const DiaryDispatchContext = React.createContext();
 
+const dummyData = [
+  {
+    id : 1,
+    emotion : 1,
+    content : "오늘의 일기 1번",
+    date : 1708433266105
+  },{
+  id : 2,
+  emotion : 2,
+  content : "오늘의 일기 2번",
+  date : 1708433266106
+},{
+  id : 3,
+  emotion : 3,
+  content : "오늘의 일기 3번",
+  date : 1708433266107
+},{
+  id : 4,
+  emotion : 4,
+  content : "오늘의 일기 4번",
+  date : 1708433266108
+}
+]
 
 function App() {
 
-  const [data, dispatch] = useReducer(reducer, []);
+  const [data, dispatch] = useReducer(reducer, dummyData);
 
   const dataId = useRef(0);
 
---
+++ src/components/DiaryList.js
@@ -0,0 +1,32 @@
+import { useState } from "react"
+const sortOptionList = [
+    {value : 'lastest', name :"최신순"},
+    {value : 'oldest', name :"오래된 순"}
+
+]
+const ControlMenu = ({value, onChange, optionList}) =>{
+    return <select value={value} onChange={(e)=> onChange(e.target.value)}>
+        {optionList.map((it,idx)=>
+            <option key={idx} value={it.value}>{it.name}</option>
+        )}
+    </select>
+}
+
+const DiaryList = ({diaryList}) => {
+    const [sortType, setSortType] = useState('lastest')
+    const getPropcessedDiaryList = ()=>{
+        
+    }
+    return(
+        <div>
+            <ControlMenu value={sortType} onChange={setSortType} optionList={sortOptionList}/>
+            {diaryList.map((it)=> <div key={it.id}>{it.content} </div>)}
+        </div>
+    )
+}
+
+DiaryList.defaultProps = {
+    diaryList : []
+}
+
+export default DiaryList
